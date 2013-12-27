32d78fcfb0bb4f7d6a0f9276b4383c5e5edd73c9
Fouppy <fred.honore@gmail.com>
2013-12-27 16:46:20 +0000

    Removed matchdep task

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -9,9 +9,6 @@
 
 module.exports = function (grunt) {
 
-    // Load grunt tasks automatically
-    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
-
     // Time how long tasks take. Can help when optimizing build times
     require('time-grunt')(grunt);
 
