516d188f8557b79c9042b0161c836958f84e2de3
Frédéric Honoré <fred.honore@gmail.com>
2014-02-28 13:09:44 +0000

    Add trailing slash using the  parameter

--- app/templates/templates/header.php
+++ app/templates/templates/header.php
@@ -1,6 +1,6 @@
 <header class="banner" role="banner">
   <div class="container">
-    <a class="brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
+    <a class="brand" href="<?php echo home_url('/') ?>"><?php bloginfo('name'); ?></a>
     <nav class="nav-main" role="navigation">
       <?php
         if (has_nav_menu('primary_navigation')) :
