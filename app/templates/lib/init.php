a399b937387b994001f6ea6178182fb752c7d2db
Frédéric Honoré <fred.honore@gmail.com>
2014-04-28 10:37:33 +0000

    Removed editor style

--- app/templates/lib/init.php
+++ app/templates/lib/init.php
@@ -20,6 +20,6 @@ function egzpo_setup() {
   // add_theme_support('post-formats', array('aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat'));
 
   // Tell the TinyMCE editor to use a custom stylesheet
-  add_editor_style('/assets/css/editor-style.css');
+  // add_editor_style('/assets/css/editor-style.css');
 }
 add_action('after_setup_theme', 'egzpo_setup');
