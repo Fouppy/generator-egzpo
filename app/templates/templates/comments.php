c172b2cb174a9f9647fe457082f6eae236ba8a3c
Fouppy <fred.honore@gmail.com>
2014-07-03 09:39:56 +0000

    Move comments.php conditionals inside <section> tags to ensure comments_link() does not break (and clean up spacing)

--- app/templates/templates/comments.php
+++ app/templates/templates/comments.php
@@ -2,9 +2,10 @@
   if (post_password_required()) {
     return;
   }
+?>
 
- if (have_comments()) : ?>
-  <section class="comments">
+<section id="comments">
+  <?php if (have_comments()) : ?>
     <h3><?php printf(_n('One Response to &ldquo;%2$s&rdquo;', '%1$s Responses to &ldquo;%2$s&rdquo;', get_comments_number(), 'egzpo'), number_format_i18n(get_comments_number()), get_the_title()); ?></h3>
 
     <ol class="media-list">
@@ -12,61 +13,65 @@
     </ol>
 
     <?php if (get_comment_pages_count() > 1 && get_option('page_comments')) : ?>
-    <nav>
-      <ul class="pager">
-        <?php if (get_previous_comments_link()) : ?>
-          <li class="previous"><?php previous_comments_link(__('&larr; Older comments', 'egzpo')); ?></li>
-        <?php endif; ?>
-        <?php if (get_next_comments_link()) : ?>
-          <li class="next"><?php next_comments_link(__('Newer comments &rarr;', 'egzpo')); ?></li>
-        <?php endif; ?>
-      </ul>
-    </nav>
+      <nav>
+        <ul class="pager">
+          <?php if (get_previous_comments_link()) : ?>
+            <li class="previous"><?php previous_comments_link(__('&larr; Older comments', 'egzpo')); ?></li>
+          <?php endif; ?>
+          <?php if (get_next_comments_link()) : ?>
+            <li class="next"><?php next_comments_link(__('Newer comments &rarr;', 'egzpo')); ?></li>
+          <?php endif; ?>
+        </ul>
+      </nav>
     <?php endif; ?>
 
     <?php if (!comments_open() && !is_page() && post_type_supports(get_post_type(), 'comments')) : ?>
-    <div class="alert">
-      <?php _e('Comments are closed.', 'egzpo'); ?>
-    </div>
+      <div class="alert alert-warning">
+        <?php _e('Comments are closed.', 'egzpo'); ?>
+      </div>
     <?php endif; ?>
-  </section><!-- /#comments -->
-<?php endif; ?>
-
-<?php if (!have_comments() && !comments_open() && !is_page() && post_type_supports(get_post_type(), 'comments')) : ?>
-  <section class="comments">
-    <div class="alert">
+  <?php elseif(!comments_open() && !is_page() && post_type_supports(get_post_type(), 'comments')) : ?>
+    <div class="alert alert-warning">
       <?php _e('Comments are closed.', 'egzpo'); ?>
     </div>
-  </section><!-- /#comments -->
-<?php endif; ?>
+  <?php endif; ?>
+</section><!-- /#comments -->
 
-<?php if (comments_open()) : ?>
-  <section class="respond">
+<section id="respond">
+  <?php if (comments_open()) : ?>
     <h3><?php comment_form_title(__('Leave a Reply', 'egzpo'), __('Leave a Reply to %s', 'egzpo')); ?></h3>
     <p class="cancel-comment-reply"><?php cancel_comment_reply_link(); ?></p>
     <?php if (get_option('comment_registration') && !is_user_logged_in()) : ?>
       <p><?php printf(__('You must be <a href="%s">logged in</a> to post a comment.', 'egzpo'), wp_login_url(get_permalink())); ?></p>
     <?php else : ?>
-      <form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" class="commentform">
+      <form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
         <?php if (is_user_logged_in()) : ?>
           <p>
             <?php printf(__('Logged in as <a href="%s/wp-admin/profile.php">%s</a>.', 'egzpo'), get_option('siteurl'), $user_identity); ?>
             <a href="<?php echo wp_logout_url(get_permalink()); ?>" title="<?php _e('Log out of this account', 'egzpo'); ?>"><?php _e('Log out &raquo;', 'egzpo'); ?></a>
           </p>
         <?php else : ?>
-          <label for="author"><?php _e('Name', 'egzpo'); if ($req) _e(' (required)', 'egzpo'); ?></label>
-          <input type="text" class="form-control" name="author" id="author" value="<?php echo esc_attr($comment_author); ?>" size="22" <?php if ($req) echo 'aria-required="true"'; ?>>
-          <label for="email"><?php _e('Email (will not be published)', 'egzpo'); if ($req) _e(' (required)', 'egzpo'); ?></label>
-          <input type="email" class="form-control" name="email" id="email" value="<?php echo esc_attr($comment_author_email); ?>" size="22" <?php if ($req) echo 'aria-required="true"'; ?>>
-          <label for="url"><?php _e('Website', 'egzpo'); ?></label>
-          <input type="url" class="form-control" name="url" id="url" value="<?php echo esc_attr($comment_author_url); ?>" size="22">
+          <div class="form-group">
+            <label for="author"><?php _e('Name', 'egzpo'); if ($req) _e(' (required)', 'egzpo'); ?></label>
+            <input type="text" class="form-control" name="author" id="author" value="<?php echo esc_attr($comment_author); ?>" size="22" <?php if ($req) echo 'aria-required="true"'; ?>>
+          </div>
+          <div class="form-group">
+            <label for="email"><?php _e('Email (will not be published)', 'egzpo'); if ($req) _e(' (required)', 'egzpo'); ?></label>
+            <input type="email" class="form-control" name="email" id="email" value="<?php echo esc_attr($comment_author_email); ?>" size="22" <?php if ($req) echo 'aria-required="true"'; ?>>
+          </div>
+          <div class="form-group">
+            <label for="url"><?php _e('Website', 'egzpo'); ?></label>
+            <input type="url" class="form-control" name="url" id="url" value="<?php echo esc_attr($comment_author_url); ?>" size="22">
+          </div>
         <?php endif; ?>
-        <label for="comment"><?php _e('Comment', 'egzpo'); ?></label>
-        <textarea name="comment" id="comment" class="form-control" rows="5" aria-required="true"></textarea>
-        <p><input name="submit" class="btn btn-primary submit" type="submit" id="submit" value="<?php _e('Submit Comment', 'egzpo'); ?>"></p>
+        <div class="form-group">
+          <label for="comment"><?php _e('Comment', 'egzpo'); ?></label>
+          <textarea name="comment" id="comment" class="form-control" rows="5" aria-required="true"></textarea>
+        </div>
+        <p><input name="submit" class="btn btn-primary" type="submit" id="submit" value="<?php _e('Submit Comment', 'egzpo'); ?>"></p>
         <?php comment_id_fields(); ?>
         <?php do_action('comment_form', $post->ID); ?>
       </form>
     <?php endif; ?>
-  </section><!-- /#respond -->
-<?php endif; ?>
+  <?php endif; ?>
+</section><!-- /#respond -->
